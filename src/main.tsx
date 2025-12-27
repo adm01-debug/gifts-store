5301cb08662b91a5390e26a4a108caadb4f8f2fb
adm01-debug <adm01@promobrindes.com.br>
2025-12-27 13:52:36 -0300

    feat: configurar locale pt-BR no entry point da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { setupLocale } from "./lib/locale-config";
+
+// Configurar locale pt-BR antes de renderizar
+setupLocale();
 
 createRoot(document.getElementById("root")!).render(<App />);
