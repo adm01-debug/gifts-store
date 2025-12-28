c9df9c8b05ce640f7997d3f78c5b8d5748934d04
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:55 -0300

    docs: add QuoteTotal.stories.tsx

---
+++ src/components/quotes/QuoteTotal.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { QuoteTotal } from '@/components/quotes/QuoteTotal';
+const meta: Meta = { title: 'Quotes/QuoteTotal', component: QuoteTotal };
+export default meta;
+export const Default = {};
