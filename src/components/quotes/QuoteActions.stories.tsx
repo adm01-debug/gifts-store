2c4310bd6ede4c1b6698291f5cc47c886f223055
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:50 -0300

    docs: add QuoteActions.stories.tsx

---
+++ src/components/quotes/QuoteActions.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { QuoteActions } from '@/components/quotes/QuoteActions';
+const meta: Meta = { title: 'Quotes/QuoteActions', component: QuoteActions };
+export default meta;
+export const Default = {};
