8274143dbde56b39243934edaf35751c86cb763d
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:53 -0300

    docs: add QuoteItems.stories.tsx

---
+++ src/components/quotes/QuoteItems.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { QuoteItems } from '@/components/quotes/QuoteItems';
+const meta: Meta = { title: 'Quotes/QuoteItems', component: QuoteItems };
+export default meta;
+export const Default = {};
