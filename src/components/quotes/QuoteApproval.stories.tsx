d4c0b550605fb5ef2f5ae410046fad1507a202d2
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:51 -0300

    docs: add QuoteApproval.stories.tsx

---
+++ src/components/quotes/QuoteApproval.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { QuoteApproval } from '@/components/quotes/QuoteApproval';
+const meta: Meta = { title: 'Quotes/QuoteApproval', component: QuoteApproval };
+export default meta;
+export const Default = {};
