e134563864a721088d31b19439dac86820698c22
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:55:13 -0300

    docs: add ClientCommunication.stories.tsx

---
+++ src/components/clients/ClientCommunication.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ClientCommunication } from '@/components/clients/ClientCommunication';
+const meta: Meta = { title: 'Clients/ClientCommunication', component: ClientCommunication };
+export default meta;
+export const Default = {};
