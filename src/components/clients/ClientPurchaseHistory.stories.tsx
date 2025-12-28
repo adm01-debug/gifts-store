7ac64acf6332d39ea8b18dd9b6133f0ca2f18706
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:55:15 -0300

    docs: add ClientPurchaseHistory.stories.tsx

---
+++ src/components/clients/ClientPurchaseHistory.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ClientPurchaseHistory } from '@/components/clients/ClientPurchaseHistory';
+const meta: Meta = { title: 'Clients/ClientPurchaseHistory', component: ClientPurchaseHistory };
+export default meta;
+export const Default = {};
