4ec9d22729a927cb727f55d943562bdbec7a334f
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:49 -0300

    docs: add ProductSearch.stories.tsx

---
+++ src/components/products/ProductSearch.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductSearch } from '@/components/products/ProductSearch';
+const meta: Meta = { title: 'Products/ProductSearch', component: ProductSearch };
+export default meta;
+export const Default = {};
