036a299b454f2fac7bfead13e5c510fc40c010a9
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:47 -0300

    docs: add ProductList.stories.tsx

---
+++ src/components/products/ProductList.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductList } from '@/components/products/ProductList';
+const meta: Meta = { title: 'Products/ProductList', component: ProductList };
+export default meta;
+export const Default = {};
