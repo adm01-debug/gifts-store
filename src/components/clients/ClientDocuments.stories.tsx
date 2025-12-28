842a4036b27f3c634166b4f85ac2c49cc192751c
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:55:14 -0300

    docs: add ClientDocuments.stories.tsx

---
+++ src/components/clients/ClientDocuments.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ClientDocuments } from '@/components/clients/ClientDocuments';
+const meta: Meta = { title: 'Clients/ClientDocuments', component: ClientDocuments };
+export default meta;
+export const Default = {};
