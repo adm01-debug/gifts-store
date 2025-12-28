5f119fa15914c74029d1dc92f99bfec55feb5937
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:23 -0300

    docs: add story Switch.stories.tsx

---
+++ src/components/ui/Switch.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Switch } from '@/components/ui/Switch';
+
+const meta: Meta<typeof Switch> = {
+  title: 'UI/Switch',
+  component: Switch,
+};
+
+export default meta;
+type Story = StoryObj<typeof Switch>;
+
+export const Default: Story = {};
