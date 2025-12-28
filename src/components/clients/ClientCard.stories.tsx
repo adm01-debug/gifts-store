59edbca2987844b4f9443a4075b42a7d7fc7ab12
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:55:12 -0300

    docs: add ClientCard.stories.tsx

---
+++ src/components/clients/ClientCard.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ClientCard } from '@/components/clients/ClientCard';
+const meta: Meta = { title: 'Clients/ClientCard', component: ClientCard };
+export default meta;
+export const Default = {};
