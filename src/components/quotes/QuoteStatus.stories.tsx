467615e4213d602da8ef33dfd01bd72637783a94
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:54 -0300

    docs: add QuoteStatus.stories.tsx

---
+++ src/components/quotes/QuoteStatus.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { QuoteStatus } from '@/components/quotes/QuoteStatus';
+const meta: Meta = { title: 'Quotes/QuoteStatus', component: QuoteStatus };
+export default meta;
+export const Default = {};
