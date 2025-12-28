eca51d1944c49ef531e680cf4a26d39ab3ce7886
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:52 -0300

    docs: add QuoteForm.stories.tsx

---
+++ src/components/quotes/QuoteForm.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { QuoteForm } from '@/components/quotes/QuoteForm';
+const meta: Meta = { title: 'Quotes/QuoteForm', component: QuoteForm };
+export default meta;
+export const Default = {};
