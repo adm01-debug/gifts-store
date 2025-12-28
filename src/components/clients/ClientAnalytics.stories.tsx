d9f64ec863d6e7f41771bd13815ebddd73b71b0b
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:55:12 -0300

    docs: add ClientAnalytics.stories.tsx

---
+++ src/components/clients/ClientAnalytics.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ClientAnalytics } from '@/components/clients/ClientAnalytics';
+const meta: Meta = { title: 'Clients/ClientAnalytics', component: ClientAnalytics };
+export default meta;
+export const Default = {};
