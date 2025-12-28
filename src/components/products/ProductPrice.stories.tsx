1e6c67ff26b231b39726e43d86012f98e49e9a4e
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:48 -0300

    docs: add ProductPrice.stories.tsx

---
+++ src/components/products/ProductPrice.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductPrice } from '@/components/products/ProductPrice';
+const meta: Meta = { title: 'Products/ProductPrice', component: ProductPrice };
+export default meta;
+export const Default = {};
