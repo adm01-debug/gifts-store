effd7305871958e99c5a16eb6753dbf7ea1e3050
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:46 -0300

    docs: add ProductFilters.stories.tsx

---
+++ src/components/products/ProductFilters.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductFilters } from '@/components/products/ProductFilters';
+const meta: Meta = { title: 'Products/ProductFilters', component: ProductFilters };
+export default meta;
+export const Default = {};
