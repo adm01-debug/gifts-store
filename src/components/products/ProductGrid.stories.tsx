f5a4a1bf6d3dd2b7d87850bb79e0aabfe4b8c792
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:46 -0300

    docs: add ProductGrid.stories.tsx

---
+++ src/components/products/ProductGrid.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductGrid } from '@/components/products/ProductGrid';
+const meta: Meta = { title: 'Products/ProductGrid', component: ProductGrid };
+export default meta;
+export const Default = {};
