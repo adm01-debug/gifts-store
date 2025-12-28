3cc95c2ee53bea0407e62c6b928cd6ea87aea224
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:21:03 -0300

    feat: add src/lib/auth/password-policy.ts

---
+++ src/lib/auth/password-policy.ts
@@ -0,0 +1,25 @@
+export const validatePassword = (password: string): { valid: boolean; errors: string[] } => {
+  const errors: string[] = [];
+
+  if (password.length < 8) {
+    errors.push('Senha deve ter no mínimo 8 caracteres');
+  }
+
+  if (!/[A-Z]/.test(password)) {
+    errors.push('Senha deve conter pelo menos uma letra maiúscula');
+  }
+
+  if (!/[a-z]/.test(password)) {
+    errors.push('Senha deve conter pelo menos uma letra minúscula');
+  }
+
+  if (!/[0-9]/.test(password)) {
+    errors.push('Senha deve conter pelo menos um número');
+  }
+
+  if (!/[!@#$%^&*]/.test(password)) {
+    errors.push('Senha deve conter pelo menos um caractere especial');
+  }
+
+  return { valid: errors.length === 0, errors };
+};
