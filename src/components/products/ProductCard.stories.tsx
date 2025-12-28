b708c5fef97d5dd2baa774a76b389335b70117af
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:44 -0300

    docs: add ProductCard.stories.tsx

---
+++ src/components/products/ProductCard.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductCard } from '@/components/products/ProductCard';
+const meta: Meta = { title: 'Products/ProductCard', component: ProductCard };
+export default meta;
+export const Default = {};
