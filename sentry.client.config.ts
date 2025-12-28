444c7aa138d9defaf2da2002d343c2c03be91225
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:20:34 -0300

    feat: add sentry.client.config.ts

---
+++ sentry.client.config.ts
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: process.env.VITE_SENTRY_DSN,
+  environment: process.env.NODE_ENV,
+  tracesSampleRate: 1.0,
+  integrations: [
+    new Sentry.BrowserTracing(),
+    new Sentry.Replay(),
+  ],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
