d1d5ff531be7a73ccad360b0b71a99bdf5b3ab1a
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:48 -0300

    docs: add ProductReviews.stories.tsx

---
+++ src/components/products/ProductReviews.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductReviews } from '@/components/products/ProductReviews';
+const meta: Meta = { title: 'Products/ProductReviews', component: ProductReviews };
+export default meta;
+export const Default = {};
