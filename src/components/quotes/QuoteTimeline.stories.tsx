ed847c362ff133817c5335b8c739da559e7f4cc2
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:55 -0300

    docs: add QuoteTimeline.stories.tsx

---
+++ src/components/quotes/QuoteTimeline.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { QuoteTimeline } from '@/components/quotes/QuoteTimeline';
+const meta: Meta = { title: 'Quotes/QuoteTimeline', component: QuoteTimeline };
+export default meta;
+export const Default = {};
