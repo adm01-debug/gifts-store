4c284d00c204e23fa5f87a40d93d9b2117a7d8cb
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:20:30 -0300

    feat: add tests/fixtures/users.ts

---
+++ tests/fixtures/users.ts
@@ -0,0 +1,14 @@
+export const mockUsers = [
+  {
+    id: 1,
+    name: 'Admin User',
+    email: 'admin@promobrindes.com.br',
+    role: 'admin',
+  },
+  {
+    id: 2,
+    name: 'Vendedor',
+    email: 'vendedor@promobrindes.com.br',
+    role: 'sales',
+  },
+];
