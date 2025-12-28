d5759b69cfc08bddc3cf6279dfc6d407e73ebd8d
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:55:15 -0300

    docs: add ClientNotes.stories.tsx

---
+++ src/components/clients/ClientNotes.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ClientNotes } from '@/components/clients/ClientNotes';
+const meta: Meta = { title: 'Clients/ClientNotes', component: ClientNotes };
+export default meta;
+export const Default = {};
