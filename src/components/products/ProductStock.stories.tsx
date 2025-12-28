7ba04a42b71c9b60a8cbdc8e576df78f4689afad
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:50 -0300

    docs: add ProductStock.stories.tsx

---
+++ src/components/products/ProductStock.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductStock } from '@/components/products/ProductStock';
+const meta: Meta = { title: 'Products/ProductStock', component: ProductStock };
+export default meta;
+export const Default = {};
