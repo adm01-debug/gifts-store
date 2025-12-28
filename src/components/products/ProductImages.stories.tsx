6b218d14d4dda2ff5452f9c437e7560b95330136
adm01-debug <adm01@promobrindes.com.br>
2025-12-28 16:54:47 -0300

    docs: add ProductImages.stories.tsx

---
+++ src/components/products/ProductImages.stories.tsx
@@ -0,0 +1,5 @@
+import type { Meta } from '@storybook/react';
+import { ProductImages } from '@/components/products/ProductImages';
+const meta: Meta = { title: 'Products/ProductImages', component: ProductImages };
+export default meta;
+export const Default = {};
