888d6ec6571a41a31cf3ccd3916d368841de2aa7
adm01-debug <adm01@promobrindes.com.br>
2026-01-02 17:04:07 -0300

    chore: cleanup storybook

--- src/components/products/ProductGrid.stories.tsx
+++
@@ -1,5 +0,0 @@
-import type { Meta } from '@storybook/react';
-import { ProductGrid } from '@/components/products/ProductGrid';
-const meta: Meta = { title: 'Products/ProductGrid', component: ProductGrid };
-export default meta;
-export const Default = {};
