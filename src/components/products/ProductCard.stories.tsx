a1c615303214edc5c2775243b854103605142311
adm01-debug <adm01@promobrindes.com.br>
2026-01-02 17:04:06 -0300

    chore: cleanup storybook

--- src/components/products/ProductCard.stories.tsx
+++
@@ -1,5 +0,0 @@
-import type { Meta } from '@storybook/react';
-import { ProductCard } from '@/components/products/ProductCard';
-const meta: Meta = { title: 'Products/ProductCard', component: ProductCard };
-export default meta;
-export const Default = {};
