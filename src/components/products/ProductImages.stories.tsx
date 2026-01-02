f8628949b13ed05ad4e466bbdf11ade32308b246
adm01-debug <adm01@promobrindes.com.br>
2026-01-02 17:04:08 -0300

    chore: cleanup storybook

--- src/components/products/ProductImages.stories.tsx
+++
@@ -1,5 +0,0 @@
-import type { Meta } from '@storybook/react';
-import { ProductImages } from '@/components/products/ProductImages';
-const meta: Meta = { title: 'Products/ProductImages', component: ProductImages };
-export default meta;
-export const Default = {};
