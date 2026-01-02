ccc43e9488dbe8dd34dd6520f6054b0f968487d7
adm01-debug <adm01@promobrindes.com.br>
2026-01-02 17:03:35 -0300

    chore: remove obsolete test

--- e2e/integrations.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('Integrations', () => {
-  test('should sync with Bitrix24', async ({ page }) => {
-    await page.goto('/settings/integrations');
-    await page.click('[data-testid="sync-bitrix"]');
-    await expect(page.locator('text=Sincronizado')).toBeVisible();
-  });
-});
