4321efd5f4d4ef9b96eca7b1c69cbe4b6391c336
adm01-debug <adm01@promobrindes.com.br>
2026-01-02 17:04:02 -0300

    chore: cleanup storybook

--- src/components/clients/ClientDocuments.stories.tsx
+++
@@ -1,5 +0,0 @@
-import type { Meta } from '@storybook/react';
-import { ClientDocuments } from '@/components/clients/ClientDocuments';
-const meta: Meta = { title: 'Clients/ClientDocuments', component: ClientDocuments };
-export default meta;
-export const Default = {};
