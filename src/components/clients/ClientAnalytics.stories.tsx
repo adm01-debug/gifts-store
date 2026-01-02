3c9bda99660faa323198a31148828c9b9f602907
adm01-debug <adm01@promobrindes.com.br>
2026-01-02 17:04:00 -0300

    chore: cleanup storybook

--- src/components/clients/ClientAnalytics.stories.tsx
+++
@@ -1,8 +0,0 @@
-// Storybook stories placeholder - requires @storybook/react
-// Component: ClientAnalytics
-
-export default {
-  title: 'Clients/ClientAnalytics',
-};
-
-export const Default = () => null;
