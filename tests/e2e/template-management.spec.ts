7c750efcc89c00a84b7e30b7f7d76eb7ad233355
adm01-debug <adm01@promobrindes.com.br>
2026-01-19 10:29:08 -0300

    rm

--- tests/e2e/template-management.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.describe('template-management', () => {
-  test('executes template-management flow', async ({ page }) => {
-    await page.goto('/');
-    expect(page).toBeDefined();
-  });
-});
